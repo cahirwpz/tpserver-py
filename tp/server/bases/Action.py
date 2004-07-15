6e2eedceff9323bc49ab4f620b3cd2bc52b4458f
Tim Ansell <tim@thousandparsec.net>
2004-07-15 17:52:32 +0000

    Only got to fix up move and combat and we have MiniSec!

---
+++ tp/server/bases/Action.py
@@ -0,0 +1,16 @@
+
+
+class Action:
+	"""\
+	Anything that happens without requiring the user to initiate.
+	Some examples would be Combat or growing the Population.
+	"""
+
+	def do(universe):
+		"""\
+		Given the universe as an interable object. It impliments most
+		functions of dictionaries.
+		A parent will appear before it's child.
+		"""
+		pass
+
