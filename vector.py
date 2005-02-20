cb099f30b304d6f93ad24a97d8fa058e3ed4d148
knio <tailor@gateway>
2005-02-20 23:37:58 +0000

    Documented vector.py a bit.
    documented vector.py a bit.

--- vector.py
+++ vector.py
@@ -5,6 +5,15 @@ import math
 
 class Vector(object):
     def __init__(self, *args):
+        '''
+        Vector([vect])
+        A 3D Vector class.
+        vect can be any one of the following:
+            an instance of Vector()
+            a tuple of cartesian coordinates, (x, y, z)
+            a tuple of spatial coordinates, ((theta, phi), magnitude)
+        represented internally as a right-handed cartesian vector
+        '''
         if len(args) == 0:
             self.x = 0.0
             self.y = 0.0
@@ -31,53 +40,56 @@ class Vector(object):
             self.cartesian(*args)
         
     def spatial(self, (a, o), m):
+        '''
+        vector.spatial((theta, phi), magnitude))
+        set the vector to the spatial angles and magnitude.
+        '''
         self.x = math.cos(a) * math.sin(o) * m
         self.y = math.sin(a) * math.sin(o) * m
         self.z = math.cos(o) * m
         
     def cartesian(self, x, y, z):
+        '''
+        vector.cartesian(x, y, z)
+        set the vector to the cartesian coordinates.
+        '''
         self.x = float(x)
         self.y = float(y)
         self.z = float(z)
         
     def distance(self, vect=None):
+        '''
+        vector.distance([vect])
+        return the distance from here to vect.
+        if vect is omitted, return the distance from the origin (the length of the vector).
+        '''
         if not isinstance(vect, Vector):    vect = Vector(vect)
         return math.sqrt((self.x-vect.x)**2 + (self.y-vect.y)**2 + (self.z-vect.z)**2)
         
     def distancexy(self, vect=None):
+        '''
+        vector.distancexy([vect])
+        return the distance from here to vect, restricted to the xy-plane.
+        if vect is omitted, return the distance from the origin on the xy-plane.
+        '''
         if not isinstance(vect, Vector):    vect = Vector(vect)
         return math.hypot(self.x-vect.x, self.y-vect.y)
         
     def angle(self, vect=None):
+        '''
+        vector.angle([vect])
+        return the angle (theta, phi) from here to vect.
+        if vect is omitted, return the angle from the origin to here.
+        '''
         if not vect: return Vector().angle(self)
         if not isinstance(vect, Vector):    vect = Vector(vect)
         return -math.atan2(self.y-vect.y, -(self.x-vect.x)), math.atan2(self.distancexy(vect), self.z-vect.z)
         
-    def add(self, vect=None):
-        if not isinstance(vect, Vector):    vect = Vector(vect)
-        self.x += vect.x
-        self.y += vect.y
-        self.z += vect.z
-        
-    def sub(self, vect=None):
-        if not isinstance(vect, Vector):    vect = Vector(vect)
-        self.x -= vect.x
-        self.y -= vect.y
-        self.z -= vect.z
-        
-    def mul(self, vect):
-        if not isinstance(vect, Vector):    vect = Vector(vect)
-        self.x *= vect.x
-        self.y *= vect.y
-        self.z *= vect.z
-
-    def div(self, vect):
-        if not isinstance(vect, Vector):    vect = Vector(vect)
-        self.x /= vect.x
-        self.y /= vect.y
-        self.z /= vect.z
-    
     def rotate(self, vect, (a, o)):
+        '''
+        vector.rotate(vect, (theta, phi))
+        rotates around the point vect, by the angles theta and phi.
+        '''
         if not isinstance(vect, Vector):    vect = Vector(vect)
         a1, o1 = vect.angle(self)
         a = a + a1
@@ -86,7 +98,51 @@ class Vector(object):
         self.add(vect)
         
     def normal(self):
+        '''
+        vector.normal()
+        normalizes the vector.
+        '''
         self.spatial(self.angle(), 1.0)
+        
+    def add(self, vect=None):
+        '''
+        vector.add([vect])
+        adds by vect.
+        '''
+        if not isinstance(vect, Vector):    vect = Vector(vect)
+        self.x += vect.x
+        self.y += vect.y
+        self.z += vect.z
+        
+    def sub(self, vect=None):
+        '''
+        vector.sub([vect])
+        subtracts by vect.
+        '''
+        if not isinstance(vect, Vector):    vect = Vector(vect)
+        self.x -= vect.x
+        self.y -= vect.y
+        self.z -= vect.z
+        
+    def mul(self, vect):
+        '''
+        vector.mul([vect])
+        multiplies by vect
+        '''
+        if not isinstance(vect, Vector):    vect = Vector(vect)
+        self.x *= vect.x
+        self.y *= vect.y
+        self.z *= vect.z
+
+    def div(self, vect):
+        '''
+        vector.div([vect])
+        divides by vect
+        '''
+        if not isinstance(vect, Vector):    vect = Vector(vect)
+        self.x /= vect.x
+        self.y /= vect.y
+        self.z /= vect.z
         
     def __iadd__(self, vect):
         self.add(vect)
