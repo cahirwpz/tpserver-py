535b538abada49a838772b57bcf0253caa004262
Tim Ansell <tim@thousandparsec.net>
2005-10-19 21:59:15 +0000

    Dump now outputs more compatible SQL. database.sql is now more compatible with other databases.

--- dump.sh
+++ dump.sh
@@ -5,7 +5,7 @@ echo "USE tp;" 				>> database.sql
 
 # Dump the database
 # We need to rewrite Universe and NOp to -1
-mysqldump -h localhost -u tp -p tp --add-drop-table -Q --skip-disable-keys --skip-add-locks --compatible=mysql323 --skip-opt \
+mysqldump -h localhost -u tp -p tp --add-drop-table -Q --skip-disable-keys --skip-add-locks --compatible=ansi,mysql323 --skip-opt \
 	| sed -e"s/(0,'sobjects.Universe','The Universe/(-1,'sobjects.Universe','The Universe/" -e's/\\n/\n/g' >> database.sql
 
 # We need to add the fix db stuff
