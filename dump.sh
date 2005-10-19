bb66afd74db4067824f3d80c6edfdf0e2eead1fc
Tim Ansell <tim@thousandparsec.net>
2005-10-19 23:33:04 +0000

    I'm going to get it right eventually...

--- dump.sh
+++ dump.sh
@@ -5,7 +5,7 @@ echo "USE tp;" 				>> ./db/database.sql
 
 # Dump the database
 # We need to rewrite Universe and NOp to -1
-mysqldump -h localhost -u tp -p tp --add-drop-table -Q --skip-disable-keys --skip-add-locks --compatible=ansi,mysql323 --skip-opt \
+mysqldump -h localhost -u tp -p tp --add-drop-table -Q --skip-disable-keys --skip-add-locks --compatible=ansi,mysql40 --skip-opt \
 	| sed -e"s/(0,'sobjects.Universe','The Universe/(-1,'sobjects.Universe','The Universe/" -e's/\\n/\n/g' >> ./db/database.sql
 
 # We need to add the fix db stuff
@@ -16,7 +16,7 @@ echo "USE tp;" 				>> ./db/database.mysql
 
 # Dump the database
 # We need to rewrite Universe and NOp to -1
-mysqldump -h localhost -u tp -p tp --add-drop-table -Q --skip-disable-keys --skip-add-locks --compatible=mysql323 --skip-opt \
+mysqldump -h localhost -u tp -p tp --add-drop-table -Q --skip-disable-keys --skip-add-locks --compatible=mysql40 --skip-opt --create-options \
 	| sed -e"s/(0,'sobjects.Universe','The Universe/(-1,'sobjects.Universe','The Universe/" -e's/\\n/\n/g' >> ./db/database.mysql
 
 # We need to add the fix db stuff
