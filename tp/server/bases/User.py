389b7b6bcfc9cc58d012417433612d9f10dcbced
Tim Ansell <tim@thousandparsec.net>
2005-12-29 11:42:43 +0000

    Added Player support.

--- tp/server/bases/User.py
+++ tp/server/bases/User.py
@@ -1,11 +1,11 @@
 
-from config import db
+from config import db, netlib
 from SQL import *
 
 class User(SQLBase):
 	tablename = "`user`"
 
-	def realid(username, password=None):
+	def usernameid(username, password=None):
 		print db
 		if password != None:
 			result = db.query("""SELECT id FROM %(tablename)s WHERE username="%(username)s" and password="%(password)s" """, username=username, password=password, tablename=User.tablename)
@@ -16,7 +16,13 @@ class User(SQLBase):
 			return -1
 		else:
 			return result[0]['id']
-	realid = staticmethod(realid)
+	usernameid = staticmethod(usernameid)
+
+	def realid(cls, user, pid):
+		if pid == 0:
+			return user.id
+		return pid
+	realid = classmethod(realid)
 
 	def __str__(self):
 		return "<User id=%s username=%s>" % (self.id, self.username)
@@ -24,3 +30,8 @@ class User(SQLBase):
 	def domain(self):
 		return self.username.split("@")[1]
 
+	def to_packet(self, sequence):
+		# Preset arguments
+		args = [sequence, self.id, self.username, ""]
+		return netlib.objects.Player(*args)
+	
