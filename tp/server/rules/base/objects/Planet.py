380c95b831f0dfe36d308ca40d529526d7ae1fdc
Tim Ansell <mithro@mithis.com>
2007-05-28 21:26:07 +0930

    Random "crazy" resources on a planet.

--- tp/server/rules/base/objects/Planet.py
+++ tp/server/rules/base/objects/Planet.py
@@ -1,11 +1,13 @@
 
+import random
+
 from tp.server.bases.Object import Object
 from tp.server.bases.Combattant import Combattant
 
 class Planet(Object, Combattant):
 	attributes = { \
-		'owner': Object.Attribute('owner', -1, 'public'),
-		'resources': Object.Attribute('resources', {}, 'public'),
+		'owner':     Object.Attribute('owner', -1, 'public'),
+		'resources': Object.Attribute('resources', {}, 'protected'),
 	}
 	orderclasses = ('tp.server.rules.base.orders.NOp', 'tp.server.rules.minisec.orders.BuildFleet')
 
@@ -37,8 +39,16 @@ class Planet(Object, Combattant):
 	def damage_get(self, fail=False):
 		return (6, 2)[fail]
 
-	def resources_get(self):
-		return []
+	def fn_resources(self, value=None):
+		r = random.Random()
+		r.seed(self.id)
+
+		res = []
+		ids = r.sample(range(0, 3), r.randint(0, 3))
+		for id in ids:
+			res.append((id, r.randint(0, 10), r.randint(0, 100), r.randint(0, 1000)))
+
+		return res
 
 Planet.typeno = 3
 Object.types[Planet.typeno] = Planet
