9aec654e446c9567df1ca5b364babd0b7fd9af5b
Tim Ansell <mithro@mithis.com>
2007-07-07 01:34:49 +0930

    Added transportable attribute. Method to get all transportable materials.

--- tp/server/rules/timtrader/bases/Resource.py
+++ tp/server/rules/timtrader/bases/Resource.py
@@ -1,4 +1,5 @@
 
+from tp.server.db import *
 
 from tp.server.bases.Resource  import Resource as ResourceBase
 
@@ -6,7 +7,8 @@ from tp.server.rules.timtrader.ProducersConsumers import split
 
 class Resource(ResourceBase):
 	attributes = { \
-		'_products': ResourceBase.Attribute('_products', {}, 'private'),
+		'_products':     ResourceBase.Attribute('_products',        {}, 'private'),
+		'transportable': ResourceBase.Attribute('transportable', False, 'private'),
 	}
 
 	def products_set(self, value):
@@ -19,3 +21,20 @@ class Resource(ResourceBase):
 			o.append((split(requirements), split(output)))
 		return o
 	products = property(products_get, products_set)
+
+	def factory(self):
+		return len(self._products) > 0
+	factory = property(factory)
+
+	# Get all resources which are transportable
+
+	def transportables(cls):
+		"""\
+		ids([user, start, amount]) -> [id, ...]
+		
+		Get the last ids for this (that the user can see).
+		"""
+		te = cls.table_extra
+		result = select([te.c.oid], (te.c.name=='transportable') & (te.c.value=='True')).execute().fetchall()
+		return [x[0] for x in result]
+	transportables = classmethod(transportables)
