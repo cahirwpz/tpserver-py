1697e6f2d843ac28bbf709bbf4fa6fbee39f68e5
Juan Lafont <juanmy777@gmail.com>
2008-06-02 12:59:50 -0400

    Copied base Planet code into dronesec Planet code

--- tp/server/rules/dronesec/objects/Planet.py
+++ tp/server/rules/dronesec/objects/Planet.py
@@ -1,5 +1,41 @@
-from tp.server.rules.base.objects.Planet import Planet as PlanetBase
 
-class Planet(PlanetBase):
-  orderclasses = ('tp.server.rules.base.orders.NOp',
+import random
+
+from tp.server.bases.Object import Object
+from tp.server.bases.Combattant import Combattant
+
+ACCESSABLE   = 0
+MINABLE      = 1
+INACCESSABLE = 2
+class Planet(Object):
+  attributes = { \
+    'owner':     Object.Attribute('owner', -1, 'public'),
+    'resources': Object.Attribute('resources', {}, 'protected'),
+    }
+  orderclasses = ('tp.server.rules.base.orders.NOp', 
     'tp.server.rules.dronesec.orders.ProduceDrones')
+
+  def ghost(self):
+    """\
+    Planets never die - even when owned by the universe.
+    """
+    return False
+
+  def fn_resources(self, value=None):
+    res = []
+    for id, values in self.resources.items():
+      res.append((id, values[0], values[1], values[2]))
+
+    return res
+
+  def resources_add(self, resource, amount, type=ACCESSABLE):
+    if not self.resources.has_key(resource):
+      self.resources[resource] = [0, 0, 0]
+    self.resources[resource][type] += amount
+
+    if self.resources[resource][type] < 0:
+      raise TypeError("Resources some how became negative!")
+
+Planet.typeno = 3
+Object.types[Planet.typeno] = Planet
+
