ee61eaee4b71648ccd018fce73ca29d3d8a1778c
Juan Lafont <juanmy777@gmail.com>
2008-06-05 19:25:43 -0400

    Fixed Build Fleet so it now builds units

--- tp/server/rules/minisec/orders/BuildFleet.py
+++ tp/server/rules/minisec/orders/BuildFleet.py
@@ -13,26 +13,26 @@ Build a new star ship fleet."""
 	typeno = 2
 
 	attributes = {\
-		'ships': Order.Attribute("ships", {}, 'protected', type=netlib.objects.constants.ARG_LIST, 
+		'ships': Order.Attribute("ships", {}, 'protected', type=netlib.objects.constants.ARG_LIST,
 					desc="Ships to build and launch."),
-		'name':  Order.Attribute("name", 'New Fleet', 'protected', type=netlib.objects.constants.ARG_STRING, 
+		'name':  Order.Attribute("name", 'New Fleet', 'protected', type=netlib.objects.constants.ARG_STRING,
 					desc="The new fleet's name.")
 	}
-	
+
 	def do(self):
 		builder = Object(self.oid)
 
 		if not hasattr(builder, "owner"):
 			print "Could not do a build order because it was on an unownable object."
 			self.remove()
-		
+
 		if self.turns() > 1:
 			# Add another year to worked...
 			self.worked += 1
 			print "Worked %s, %s left until built." % (self.worked, self.turns())
 			self.save()
 			return
-			
+
 		# Build new fleet object
 		fleet = Object(type='tp.server.rules.minisec.objects.Fleet')
 
@@ -52,7 +52,7 @@ Build a new star ship fleet."""
 		message.slot = -1
 		message.bid = builder.owner
 		message.subject = "Fleet built"
-		
+
 		message.body = """\
 A new fleet (%s) has been built and is orbiting %s.
 It consists of:
@@ -70,7 +70,7 @@ It consists of:
 
 	def turns(self, turns=0):
 		time = {0:1, 1:2, 2:4}
-	
+
 		for type, number in self.ships.items():
 			turns += time[type] * number
 
@@ -83,7 +83,7 @@ It consists of:
 		if value == None:
 			returns = []
 			for type, name in Fleet.ship_types.items():
-				returns.append((type, name, -1))
+				returns.append((type, name, 10))
 			return returns, self.ships.items()
 		else:
 			ships = {}
