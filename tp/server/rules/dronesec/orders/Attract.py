33de29107d47fe59eb0ecdc5370eb36a0b241422
Juan Lafont <juanmy777@gmail.com>
2008-06-12 13:23:02 -0400

    Attract Command implemented

---
+++ tp/server/rules/dronesec/orders/Attract.py
@@ -0,0 +1,38 @@
+
+from tp import netlib
+
+from tp.server.bases.Order import Order
+from tp.server.bases.Object import Object
+from tp.server.bases.Message import Message
+from tp.server.rules.dronesec.objects.overlord.Messenger import Messenger
+
+
+class Attract(Order):
+	"""\
+Drones move to a point in space.
+"""
+	typeno = 3
+	attributes = {\
+		'pos': Order.Attribute("pos", (0,0,0), 'public', type=netlib.objects.constants.ARG_ABS_COORD,
+				desc="Where to go.")
+	}
+
+	def do(self):
+		obj = Object(self.oid)
+		if not hasattr(obj, "command"):
+			print "Could not assign move command because object is not an Overlord"
+			self.remove()
+			return
+
+
+		obj.pos = self.pos
+		obj.command = 1
+		obj.save()
+		self.remove()
+
+
+	def turns(self, turns=0):
+		return turns
+
+	def resources(self):
+		return []
