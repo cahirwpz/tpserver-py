b8ca03e6a8ebe494b6138d54f1de6fdfae006f4d
Juan Lafont <juanmy777@gmail.com>
2008-06-12 13:25:27 -0400

    Overlords now have Command orders

--- tp/server/rules/dronesec/objects/overlord/Fleet.py
+++ tp/server/rules/dronesec/objects/overlord/Fleet.py
@@ -1,4 +1,4 @@
-
+###Overlord Fleet
 from sqlalchemy import *
 
 from types import TupleType, ListType
@@ -6,13 +6,29 @@ from types import TupleType, ListType
 from tp.server.bases.Object import Object
 from tp.server.bases.Design import Design
 from tp.server.rules.dronesec.objects.Fleet import Fleet as Fleetbase
+from Messenger import Messenger as Jarvis
 
 class Fleet(Fleetbase):
+	attributes = { \
+		'owner': Object.Attribute('owner', -1, 'public'),
+		'ships': Object.Attribute('ships', {}, 'protected'),
+		'damage': Object.Attribute('damage', {}, 'protected'),
+		'pos' : Object.Attribute('pos', (0,0,0), 'private'),
+		'command' : Object.Attribute('command', 0, 'private'),
+	}
+
+	##Commands:
+	## 0 : Stop
+	## 1 : Attract
+	## 2 : Repel
+
 	orderclasses = (
 		'tp.server.rules.base.orders.NOp',
-		'tp.server.rules.minisec.orders.Move',
+		'tp.server.rules.dronesec.orders.Move',
+		'tp.server.rules.dronesec.orders.Attract',
+		'tp.server.rules.dronesec.orders.Repel',
+		'tp.server.rules.dronesec.orders.Stop',
 		)
-
 
 	ship_types = {0: 'Overlord'}
 	ship_hp     = {0: 10000}
