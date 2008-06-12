59096606155f92c7489ab1b42280954e3f090de2
Juan Lafont <juanmy777@gmail.com>
2008-06-12 13:28:12 -0400

    Created own move action that would only move Overlords

---
+++ tp/server/rules/dronesec/actions/Move.py
@@ -0,0 +1,20 @@
+"""\
+Moves any object which has a velocity.
+"""
+
+from tp.server.utils import WalkUniverse, ReparentOne
+
+def do(top):
+	def move(obj):
+		if (obj.velx, obj.vely, obj.velz) != (0,0,0) and hasattr(obj, "command"):
+			print "Moving object %s from (%s, %s, %s) to (%s, %s, %s)" % (
+				obj.id,
+				obj.posx, obj.posy, obj.posz,
+				obj.posx + obj.velx, obj.posy + obj.vely, obj.posz + obj.velz)
+
+			obj.posx, obj.posy, obj.posz = obj.posx + obj.velx, obj.posy + obj.vely, obj.posz + obj.velz
+			# FIXME: This could be dangerous
+			ReparentOne(obj)
+			obj.save()
+
+	WalkUniverse(top, "after", move)
