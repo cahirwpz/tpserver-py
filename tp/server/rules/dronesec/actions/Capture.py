aa27d63838f553b1cfcf05290f16da31e5fef439
Juan Lafont <juanmy777@gmail.com>
2008-06-17 15:26:37 -0400

    Added some documentation to Capture

--- tp/server/rules/dronesec/actions/Capture.py
+++ tp/server/rules/dronesec/actions/Capture.py
@@ -11,11 +11,14 @@ from tp.server.bases.Message import Message
 def do(top):
 	def h(obj):
 		if obj.type.endswith("Planet"):
+			#Build list of fleets that could capture the planet
 			capturers = Object.bypos([obj.posx, obj.posy, obj.posz], size=obj.size+2)
 			capturers  = [id for (id, time) in capturers if Object(id).type ==
 				'tp.server.rules.dronesec.objects.Fleet']
+			#If there are any capturers present.
 			if capturers:
 				checkOwners = dict()
+				#Checks to see how many players are at the planet and their total power
 				for id in capturers:
 					if hasattr(Object(id), 'target'):
 						i = Object(id).owner
@@ -24,13 +27,18 @@ def do(top):
 							checkOwners[i] += power
 						except:
 							checkOwners[i] = power
+				#A conqueror is the only person with a chance to capture a planet.
+				#A conqueror is the player with the most power at the planet
 				conqueror = max(checkOwners,key = lambda a: checkOwners.get(a))
 				conqpow = checkOwners[conqueror]
+				#Conquerors should not already own the planet
 				if conqueror != obj.owner:
 					if len(checkOwners) > 1:
 						for id, pow in checkOwners:
 							if id != conqueror:
 								conqpow -= pow
+					# To Capture a planet a conqueror must have 50 power more
+					# than all other players at that planet
 					if conqpow >= 50:
 						obj.owner = conqueror
 						#Message
@@ -46,6 +54,8 @@ Capture of %s <b>succeded</b>.""" % (obj.name,)
 						#remove Drones
 						powerLoss = 0
 						for id in capturers:
+							# This should go through the fleets taking away the
+							# ships with the smallest amount of power first.
 							if powerLoss < 50:
 								drone = Object(id)
 								if drone.owner == conqueror and hasattr(drone, 'target'):
