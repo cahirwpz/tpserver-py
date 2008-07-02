60c082c0133ddef95142b583f5b763a378fcfb71
Juan Lafont <juanmy777@gmail.com>
2008-07-02 18:03:21 -0400

    Added a byreq function to the Research SQL type

--- tp/server/rules/dronesec/bases/Research.py
+++ tp/server/rules/dronesec/bases/Research.py
@@ -18,7 +18,7 @@ class Research(SQLTypedBase):
 		Column('abbrev',       Binary,   nullable=False, default = ''),
 		Column('cost',         Integer,   nullable= False, default= 0),
 		Column('reqs',         PickleType,   nullable=False, default = []),
-		Column('desc',         Binary,   nullable=False, default = 'gah'),
+		Column('desc',         Binary,   nullable=False, default = ''),
 		Column('time',	    DateTime,    nullable=False, index=True,
 			onupdate=func.current_timestamp(), default=func.current_timestamp()),
 
@@ -45,6 +45,17 @@ class Research(SQLTypedBase):
 
 		# FIXME: Need to figure out what is going on here..
 		results = select([t.c.id], (t.c.type==bindparam('type'))).execute(type=type).fetchall()
+		return [(x['id']) for x in results]
+
+	@classmethod
+	def byreq(cls, abbrev):
+		"""\
+		byreq(abbrev)
+
+		Returns the objects which contain this research abbrev as a requirement
+		"""
+		c = cls.table.c
+		results = select([c.id], c.reqs.in_(abbrev)).execute().fetchall()
 		return [(x['id']) for x in results]
 
 
