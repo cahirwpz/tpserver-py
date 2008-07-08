d834ef2428594050d4bf0b35154c7d04f4f36f76
Juan Lafont <juanmy777@gmail.com>
2008-07-08 15:43:25 -0400

    World Type Researches implemented with some speed bonus researchs as well.

--- tp/server/rules/dronesec/research/WorldType.py
+++ tp/server/rules/dronesec/research/WorldType.py
@@ -1,10 +1,17 @@
 
-from Research import Research
+from tp.server.rules.dronesec.bases.Research import Research
 
 class WorldType(Research):
-	speed = 0
-	speedRatio = 0
-	units = None
-	type = None
-	abilities = None
-	power = 0
+##	speed = 0
+##	speedRatio = 0
+##	units = None
+##	type = None
+##	abilities = None
+##	power = 0
+	attributes = {\
+		'speed': Research.Attribute('speed', 0, 'private'),
+		'speedRatio': Research.Attribute('speedRatio', 0, 'private'),
+		'power': Research.Attribute('power', 0, 'private'),
+		'units': Research.Attribute('units', '', 'private'),
+		'types': Research.Attribute('types', '', 'private'),
+		}
