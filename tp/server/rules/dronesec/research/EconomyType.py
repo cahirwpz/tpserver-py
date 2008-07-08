0b9c9f9add22cbcba9c67a07cc19448c2d441d7b
Juan Lafont <juanmy777@gmail.com>
2008-07-08 15:51:05 -0400

    Economy Research types now use SQL table

--- tp/server/rules/dronesec/research/EconomyType.py
+++ tp/server/rules/dronesec/research/EconomyType.py
@@ -1,5 +1,5 @@
 
-from Research import Research
+from tp.server.rules.dronesec.bases.Research import Research
 
 class EconomyType(Research):
 	resources = 0
@@ -7,3 +7,10 @@ class EconomyType(Research):
 	researchType = 0
 	resourcesRatio = 0
 	researchRatio = 0
+	attributes = {\
+		'resources': Research.Attribute('resources', 0, 'private'),
+		'research': Research.Attribute('research', 0, 'private'),
+		'researchType': Research.Attribute('researchType', 0, 'private'),
+		'resourcesRatio': Research.Attribute('resourcesRatio', 0, 'private'),
+		'researchRatio': Research.Attribute('researchRatio', 0, 'private'),
+	}
