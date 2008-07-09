d02198655584fa215c8022664a1448467ea2f754
Juan Lafont <juanmy777@gmail.com>
2008-07-09 16:09:25 -0400

    Removed some comments from World Researches

--- tp/server/rules/dronesec/research/WorldType.py
+++ tp/server/rules/dronesec/research/WorldType.py
@@ -2,12 +2,6 @@
 from tp.server.rules.dronesec.bases.Research import Research
 
 class WorldType(Research):
-##	speed = 0
-##	speedRatio = 0
-##	units = None
-##	type = None
-##	abilities = None
-##	power = 0
 	attributes = {\
 		'speed': Research.Attribute('speed', 0, 'private'),
 		'speedRatio': Research.Attribute('speedRatio', 0, 'private'),
