b535d13b4feed1c3345e2052bca8d12b4743ad72
Juan Lafont <juanmy777@gmail.com>
2008-07-09 16:09:09 -0400

    Fixes to economy Researches

--- tp/server/rules/dronesec/research/EconomyType.py
+++ tp/server/rules/dronesec/research/EconomyType.py
@@ -2,15 +2,9 @@
 from tp.server.rules.dronesec.bases.Research import Research
 
 class EconomyType(Research):
-	resources = 0
-	research = 0
-	researchType = 0
-	resourcesRatio = 0
-	researchRatio = 0
 	attributes = {\
 		'resources': Research.Attribute('resources', 0, 'private'),
-		'research': Research.Attribute('research', 0, 'private'),
-		'researchType': Research.Attribute('researchType', 0, 'private'),
 		'resourcesRatio': Research.Attribute('resourcesRatio', 0, 'private'),
 		'researchRatio': Research.Attribute('researchRatio', 0, 'private'),
+		'researchType': Research.Attribute('researchType', 0, 'private'),
 	}
