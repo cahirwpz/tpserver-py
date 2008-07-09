d64c4db4fd893c7ed3d88d7763e19e6c34c35844
Juan Lafont <juanmy777@gmail.com>
2008-07-09 15:22:49 -0400

    Planets shouls now produce more resources based on Research

--- tp/server/rules/dronesec/actions/AddResource.py
+++ tp/server/rules/dronesec/actions/AddResource.py
@@ -6,13 +6,31 @@ from tp.server.bases.Object import Object
 from tp.server.utils import WalkUniverse
 from tp.server.bases.Resource import Resource
 from tp.server.rules.dronesec.objects.Planet import Planet
+from tp.server.rules.dronesec.bases.Research import Research
+from tp.server.rules.dronesec.bases.Player import Player
+
+
 
 def do(top):
 	def h(obj):
 		if obj.type.endswith("Planet"):
 			if obj.owner != -1:
-					obj.resources_add(Resource.byname('Credit'), 100)
-					print "%s is  producing 100 Credits" % (obj.name)
-					obj.damage = {}
-					obj.save()
+				resources = 100
+				resourceRatio = 1
+				researches = Research.bytype('tp.server.rules.dronesec.research.EconomyType')
+				res = []
+				for x in Player(obj.owner).research:
+					if x in researches:
+						res.append(x)
+				researches = res
+				if researches:
+					for id in researches:
+						resources += Research(id).resources
+						resourceRatio += Research(id).resourceRatio
+				resources = resource * resourceRatio
+				obj.resources_add(Resource.byname('Credit'), resources)
+				print "%s is  producing %s Credits" % (obj.name, resources)
+
+				obj.damage = {}
+				obj.save()
 	WalkUniverse(top, "before", h)
