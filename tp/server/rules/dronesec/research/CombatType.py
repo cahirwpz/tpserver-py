7881b0ed2a4f1a1405f6db93d74ec001bda7d398
Juan Lafont <juanmy777@gmail.com>
2008-07-10 18:40:29 -0400

    New Researches have been added.

--- tp/server/rules/dronesec/research/CombatType.py
+++ tp/server/rules/dronesec/research/CombatType.py
@@ -7,6 +7,8 @@ class CombatType(Research):
 		'damage': Research.Attribute('damage', 0, 'private'),
 		'numAttacks': Research.Attribute('numAttacks', 0, 'private'),
 		'health': Research.Attribute('health', 0, 'private'),
+		'strength': Research.Attribute('strength', '', 'private'),
+		'weakness': Research.Attribute('weakness', '', 'private'),
 		'types': Research.Attribute('types', 0, 'private'),
 		'ships': Research.Attribute('ships', 0, 'private'),
 		}
