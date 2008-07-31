06cad5a8288584b7842d65667ac655cbaf5af53e
Juan Lafont <juanmy777@gmail.com>
2008-07-31 15:35:15 -0400

    Refactored AddResource to use the ResearchCalculator

--- tp/server/rules/dronesec/actions/AddResource.py
+++ tp/server/rules/dronesec/actions/AddResource.py
@@ -5,28 +5,16 @@ Adds the necesary resources to each planet
 from tp.server.bases.Object import Object
 from tp.server.utils import WalkUniverse
 from tp.server.bases.Resource import Resource
-from tp.server.rules.dronesec.objects.Planet import Planet
-from tp.server.rules.dronesec.bases.Research import Research
-from tp.server.rules.dronesec.bases.Player import Player
-
+from tp.server.rules.dronesec.research.ResearchCalculator import ResearchCalculator as RC
 
 
 def do(top):
 	def h(obj):
 		if obj.type.endswith("Planet"):
 			if obj.owner != -1:
-				resources = 100
-				resourceRatio = 1
-				researches = Research.bytype('tp.server.rules.dronesec.research.EconomyType')
-				res = []
-				for x in Player(obj.owner).research:
-					if x in researches:
-						res.append(x)
-				researches = res
-				if researches:
-					for id in researches:
-						resources += Research(id).resources
-						resourceRatio += Research(id).resourcesRatio
+				resources, resourceRatio, researchRatio = RC.Economy(obj.owner)
+				resources += 100
+
 				resources = resources * resourceRatio
 				obj.resources_add(Resource.byname('Credit'), resources)
 				if obj.resources[Resource.byname('Credit')][0] > 100000:
