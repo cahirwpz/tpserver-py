684d2aec86e1334f9c4b76facbc164eee78aef0a
Krystian Bacławski <krystian.baclawski@gmail.com>
2010-05-27 20:30:32 +0200

    Added feature - create packet from objects.

--- tp/server/packet.py
+++ tp/server/packet.py
@@ -46,7 +46,104 @@ class PacketFactory( object ):#{{{
 			packet = None
 
 		return packet
+
+	def fromObject( self, packet, seq, obj ):
+		return getattr( self, "make%sPacket" % packet )( seq, obj )
+
+	@property
+	def server_version( self ):
+		return "%d.%d.%d" % (0,4,0)
+
+	@property
+	def server_software( self ):
+		return "tpserver-py"
+
+	@property
+	def locations( self ):
+		servername	= "localhost"
+		serverip	= "127.0.0.1"
+		port		= "6923"
+
+		locations = []
+		locations.append(('tp',       servername, serverip, port))
+		#locations.append(('tp+http',  servername, serverip, port))
+		#locations.append(('tps',      servername, serverip, port))
+		#locations.append(('tp+https', servername, serverip, port))
+
+	def makeGamePacket( self, seq, obj ):
+		return self.objects.Game( seq, obj.longname, obj.key, ["0.3", "0.3+"],
+				self.server_version, self.server_software, obj.ruleset.name,
+				obj.ruleset.version, self.locations, obj.parameters)
+
+	def makeCategoryPacket( self, seq, obj ):
+		return self.objects.Category( seq, obj.id, obj.time, obj.name, obj.desc )
+	
+	def makeComponentPacket( self, seq, obj ):
+		return self.objects.Component( seq, obj.id, obj.time, obj.categories,
+				obj.name, obj.desc, obj.requirements, obj.properties )
+
+	def makeDesignPacket( self, seq, obj ):
+		return self.objects.Design( seq, obj.id, obj.time, obj.categories,
+				obj.name, obj.desc, obj.used, obj.owner, obj.components,
+				obj.feedback, obj.properties)
+
+	def makeBoardPacket( self, seq, obj ):
+		return self.objects.Board( seq, Board.mangleid( obj.id ), obj.name,
+				obj.desc, Message.number( obj.id ), self.time)
+
+	def makeMessagePacket( self, seq, obj):
+		# FIXME: The reference system needs to be added and so does the turn
+		return self.objects.Message( seq, obj.bid, obj.slot, [], obj.subject,
+				obj.body, 0, [])
+
+	def makeObjectPacket( self, seq, obj ):
+		return self.objects.Object( seq, obj.id, obj.typeno, obj.name,
+				"Description", 1, obj.contains, 2, 3, [ [[obj.posx, obj.posy,
+					obj.posz], -1], [[obj.velx, obj.vely, obj.velz], -1] ] )
+				# 1 => parent, 2 => modtime, 3 => padding
+
+	def makePropertyPacket( self, seq, obj ):
+		return self.objects.Property( seq, obj.id, obj.time, obj.categories,
+				obj.rank, obj.name, obj.displayname, obj.desc, obj.calculate,
+				obj.requirements)
+
+	def makeResourcePacket( self, seq, obj ):
+		return self.objects.Resource( seq, obj.id, obj.namesingular,
+				obj.nameplural, obj.unitsingular, obj.unitplural, obj.desc,
+				obj.weight, obj.size, obj.time)
+
+	def makePlayerPacket( self, seq, obj ):
+		return self.objects.Player( seq, obj.id, obj.username, "" )
+
+	def makeObjectIDsPacket( self, seq, obj ):
+		return self.objects.ObjectIDs( seq, obj.key, obj.left, obj.ids, -1 )
 #}}}
+
+"""
+	# Taken from Design
+	def from_packet(self, user, packet):
+		# Check the design meets a few guide lines
+		
+		# FIXME: Check each component exists and the amount is posative
+		for id, amount in packet.components:
+			pass
+		
+		# Check we have at least one category
+		if len(packet.categories) < 1:
+			raise ValueError("Designs must have atleast one category")
+		else:
+			# FIXME: Check that the categories are valid
+			pass
+		
+		# FIXME: Check the owner is sane
+	
+		# FIXME: Check the id
+		if packet.id != -1:
+			self.id = packet.id
+			
+		for key in ["categories", "name", "desc", "owner", "components"]:
+			setattr(self, key, getattr(packet, key))
+"""
 
 def PacketFormatter( packet ):#{{{
 	attrs = []
