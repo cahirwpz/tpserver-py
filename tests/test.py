43892364ba9475b4b46831e5efd806bfb68d4fa0
Krystian Bacławski <krystian.baclawski@gmail.com>
2010-08-04 14:35:52 +0200

    Further improvements of server test framework.
      * Properly implemented TestCase execution.
      * Exception thrown out in SetUp and TearDown methods
        are intercepted.
      * TestSuite behaves like TestCase container.
      * Renamed TestManager to TestLoader and moved to test.py.
      * Generalization and refactoring of TestLoader class.
      * Renamed TestSelector to MainTestSuite.
      * Temporarily disabled test selection facility.

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import textwrap, glob, os.path, traceback
+from collections import Mapping
+
+from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 
-from tp.server.logging import msg
+from tp.server.logging import msg, err, logctx
 
 class TestCase( object ):#{{{
 	def __init__( self ):
@@ -10,40 +14,73 @@ class TestCase( object ):#{{{
 		self.reason = None
 		self.result = Deferred()
 
+	def __setUp( self ):
+		try:
+			self.setUp()
+		except Exception, ex:
+			err()
+
+			try:
+				self.tearDown()
+			except Exception, ex:
+				err()
+
+			self.status = False
+			self.reason = "SetUp method failed!"
+			self.__tearDown()
+		else:
+			reactor.callLater( 0, self.run )
+
+	def __tearDown( self ):
+		try:
+			self.tearDown()
+		except Exception, ex:
+			err()
+
+			self.status = False
+			self.reason = "TearDown method failed!"
+
+		if self.status:
+			self.result.callback( self )
+		else:
+			self.result.errback( self )
+
 	def setUp( self ):
 		pass
+
+	def run( self ):
+		self.succeeded()
 
 	def tearDown( self ):
 		pass
 
-	def run( self ):
-		raise NotImplementedError
-
-	def __call__( self ):
-		self.run()
+	@logctx
+	def start( self ):
+		self.__setUp()
 
 	def succeeded( self ):
 		self.status = True
-		self.result.callback( self )
+		reactor.callLater( 0, self.__tearDown )
 
 	def failed( self, reason ):
 		self.status = False
 		self.reason = reason
-		self.result.errback( self )
+		reactor.callLater( 0, self.__tearDown )
 
 	def logPrefix( self ):
 		try:
-			return self.parent.__name__
+			return self.__parent__.__name__
 		except AttributeError:
 			return self.__class__.__name__
 #}}}
 
-class TestSuite( TestCase ):#{{{
+class TestSuite( Mapping, TestCase ):#{{{
 	def __init__( self ):
 		super( TestSuite, self ).__init__()
 
 		self.__tests = []
-		self.__index = 0
+		self.__names = {}
+		self.__iter  = None
 	
 	def setUp( self ):
 		msg( "${cyn1}Setting up %s test suite...${coff}" % self.__class__.__name__, level='info' ) 
@@ -53,36 +90,106 @@ class TestSuite( TestCase ):#{{{
 
 	def addTest( self, *args ):
 		for cls in args:
-			self.__tests.append( cls )
+			if cls in self.__tests:
+				msg( '${yel1}Test of type %s already registered!${coff}' % cls.__name__ )
+			else:
+				self.__tests.append( cls )
+				self.__names[ cls.__name__ ] = cls
+
+				cls.__parent__ = self
+
+	def __getitem__( self, name ):
+		return self.__names[ name ]
+
+	def __len__( self ):
+		return self.__names.__len__()
+
+	def __iter__( self ):
+		return self.__names.iterkeys()
+
+	def __contains__( self, test ):
+		if issubclass( test, TestCase ):
+			return test in self.__tests
+
+		return test in self.__names
 
 	def run( self ):
-		if self.__index >= len( self.__tests ):
+		if not self.__iter:
+			self.__iter = iter( self.__tests )
+
+		try:
+			TestType = self.__iter.next()
+		except StopIteration:
 			if self.result:
 				self.result.callback( self )
 			else:
 				self.result.errback( self )
 		else:
-			TestType = self.__tests[ self.__index ]
-			TestType.parent = self
-
-			self.__index += 1
-
 			test = TestType()
 			test.result.addCallbacks( self.__succeeded, self.__failed )
-			test.setUp()
-			test.run()
+			test.start()
 
 	def __succeeded( self, test ):
-		test.tearDown()
 		self.run()
 
 	def __failed( self, failure ):
 		self.status = False
 		self.reason = "One or more tests failed!"
-
-		test = failure.value
-		test.tearDown()
 		self.run()
+
+	def getListing( self ):
+		"""
+		Retrieves report of all available tests.
+		"""
+
+		textwrapper = textwrap.TextWrapper()
+		textwrapper.initial_indent = ' ' * 4
+		textwrapper.subsequent_indent = ' ' * 11
+		textwrapper.width = 100
+
+		report = []
+
+		report.append( '${cyn0}Available test cases list:${coff}' )
+
+		for name, cls in sorted( self.__names.iteritems() ):
+			report.append( '  ${wht1}%s:${coff}' % name )
+
+			words = str( cls.__doc__ ).split()
+
+			report += textwrapper.wrap( '${yel0}info:${coff}  %s' % ' '.join( words ) )
+
+		report.append( '${cyn0}Available test cases count: %d${coff}' % len( self ) )
+
+		return report
 #}}}
 
-__all__ = [ 'TestCase', 'TestSuite' ]
+class TestLoader( TestSuite ):#{{{
+	"""
+	Stores and manages all tests.
+	"""
+
+	__path__ = None
+
+	def __init__( self ):
+		super( TestLoader, self ).__init__()
+
+		path = os.path.dirname( os.path.abspath( __file__ ) )
+
+		for modulePath in sorted( glob.glob( os.path.join( path, self.__path__, '*.py' ) ) ):
+			moduleName = os.path.splitext( os.path.basename( modulePath ) )[0]
+
+			try:
+				module = __import__( "%s.%s" % ( self.__path__, moduleName ), globals(), locals(), [ moduleName ], -1 )
+
+				try:
+					classes = module.__tests__
+				except AttributeError:
+					classes = []
+
+				self.addTest( *classes )
+			except ImportError, msg:
+				print "Could not import %s: %s" % ( moduleName, msg )
+				traceback.print_exc()
+#}}}
+
+__all__ = [ 'TestCase', 'TestSuite', 'TestLoader' ]
--- tests/testrunner.py
+++ tests/testrunner.py
@@ -1,59 +1,62 @@
 #!/usr/bin/env python
 
-import fnmatch
-
 from twisted.internet import reactor
+from tp.server.gamemanager import GameManager
 
-from tp.server.logging import Logger, msg, logctx
-from tp.server.configuration import ComponentConfiguration, StringOption, ConfigurationError
+from test import TestLoader
 
-from test import TestSuite
+class MainTestSuite( TestLoader ):#{{{
+	__path__ = 'testcases'
 
-class TestSelector( TestSuite ):#{{{
-	def __init__( self ):
-		super( TestSelector, self ).__init__()
+	def setUp( self ):
+		minisec		= 'test_minisec'
+		minisecplus = 'test_minisecplus'
 
-		from testcases import __manager__ as testcases
+		if minisec not in GameManager():
+			GameManager().addGame( minisec, 'Test Game (minisec)',
+				'minisec', 'admin@localhost', 'Test game used for testing purposes')
 
-		for name, cls in testcases.iteritems():
-			self.addTest( cls )
+		if minisecplus not in GameManager():
+			GameManager().addGame( minisecplus, 'Test Game (minisecplus)',
+				'minisecplus', 'admin@localhost', 'Test game used for testing purposes')
 
-	def configure( self, configuration ):
-		tests = configuration.tests
+		GameManager()[ minisec ]
+		GameManager()[ minisecplus ]
 
-		if tests == 'LIST':
-			raise SystemExit( Logger.colorizeMessage( '\n'.join( self.__manager.getReport() ) ) )
-		elif tests == 'DEFAULT':
-			self.__tests = [ 'DEFAULT' ]
-		elif tests == 'ALL':
-			#self.__tests = self.__manager.keys()
-			pass
-		else:
-			for name in tests.split(','):
-				try:
-					names = fnmatch.filter( self.__manager.keys(), name )
-
-					if not names:
-						raise KeyError( 'No test matching \'%s\'' % name )
-
-					self.__tests.extend( names )
-				except KeyError, ex:
-					raise SystemExit( ex )
-
-		#if not self.__tests:
-		#	raise ConfigurationError( 'test run is empty' )
-
+	#def configure( self, configuration ):
+	#	tests = configuration.tests
+	#
+	#	if tests == 'LIST':
+	#		raise SystemExit( Logger.colorizeMessage( '\n'.join( self.__manager.getReport() ) ) )
+	#	elif tests == 'DEFAULT':
+	#		self.__tests = [ 'DEFAULT' ]
+	#	elif tests == 'ALL':
+	#		self.__tests = self.__manager.keys()
+	#	else:
+	#		for name in tests.split(','):
+	#			try:
+	#				names = fnmatch.filter( self.__manager.keys(), name )
+	#
+	#				if not names:
+	#					raise KeyError( 'No test matching \'%s\'' % name )
+	#
+	#				self.__tests.extend( names )
+	#			except KeyError, ex:
+	#				raise SystemExit( ex )
+	#
+	#	if not self.__tests:
+	#		raise ConfigurationError( 'test run is empty' )
 #}}}
 
-class TestSelectorConfiguration( ComponentConfiguration ):#{{{
-	tests = StringOption( short='t', default='ALL',
-						  help='Specifies which tests will be added to test run. TEST-LIST is a list of test names or glob (see unix manual pages) patterns separated by comma. There are some arguments to this option that have a special meaning. \'LIST\' will force to display all available tests and finish the application. \'ALL\' will add all available test to test run.', arg_name='TEST-LIST' )
+#class MainTestSuiteConfiguration( ComponentConfiguration ):#{{{
+#	tests = StringOption( short='t', default='ALL',
+#						  help='Specifies which tests will be added to test run. TEST-LIST is a list of test names or glob (see unix manual pages) patterns separated by comma. There are some arguments to this option that have a special meaning. \'LIST\' will force to display all available tests and finish the application. \'ALL\' will add all available test to test run.', arg_name='TEST-LIST' )
 #}}}
 
 class TestRunner( object ):#{{{
-	def __init__( self, selector ):
-		self.selector = selector
-		self.selector.result.addCallbacks( self.__succeeded, self.__failed )
+	def __init__( self ):
+		self.suite = MainTestSuite()
+		self.suite.result.addCallbacks( self.__succeeded, self.__failed )
 	
 	def __succeeded( self, test ):
 		reactor.stop()
@@ -62,10 +65,10 @@ class TestRunner( object ):#{{{
 		reactor.stop()
 
 	def start( self ):
-		reactor.callLater( 0, self.selector.run )
+		reactor.callLater( 0, self.suite.start )
 	
 	def logPrefix( self ):
 		return self.__class__.__name__
 #}}}
 
-__all__ = [ 'TestRunner', 'TestSelector', 'TestSelectorConfiguration' ]
+__all__ = [ 'TestRunner' ]
