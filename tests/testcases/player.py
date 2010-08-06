41854d0cba3975fd7b17e92c257139efe3aba3ce
Krystian Bacławski <krystian.baclawski@gmail.com>
2010-08-06 15:20:01 +0200

    setUp / tearDown for Player related tests.

--- tests/testcases/player.py
+++ tests/testcases/player.py
@@ -5,52 +5,60 @@ class GetCurrentPlayer( AuthorizedTestSession ):
 	""" Does server respond with current player information? """
 
 	def setUp( self ):
-		self.login		= self.ctx['player2'].username
-		self.password	= self.ctx['player2'].password
+		self.login		= self.ctx['players'][1].username
+		self.password	= self.ctx['players'][1].password
 
 	def __iter__( self ):
+		player = self.ctx['players'][1]
+
 		GetPlayer = self.protocol.use( 'GetPlayer' )
 
 		packet = yield GetPlayer( self.seq, [0] ), Expect( 'Player' )
 
-		if packet.id != 2:
+		if packet.id != player.id:
 			self.failed( "Server responded with different PlayerId than requested!" )
 
 class GetExistingPlayer( AuthorizedTestSession ):
 	""" Does server respond properly if asked about existing player? """
 
 	def __iter__( self ):
+		player = self.ctx['players'][1]
+
 		GetPlayer = self.protocol.use( 'GetPlayer' )
 
-		packet = yield GetPlayer( self.seq, [1] ), Expect( 'Player' )
+		packet = yield GetPlayer( self.seq, [ player.id ] ), Expect( 'Player' )
 
-		if packet.id != 1:
+		if packet.id != player.id:
 			self.failed( "Server responded with different PlayerId than requested!" )
 
 class GetNonExistentPlayer( AuthorizedTestSession ):
 	""" Does server fail to respond if asked about nonexistent player? """
 
 	NoFailAllowed = False
-	WrongPlayerId = 666
 
 	def __iter__( self ):
+		player = self.ctx['players'][1]
+
 		GetPlayer = self.protocol.use( 'GetPlayer' )
 
-		packet = yield GetPlayer( self.seq, [self.WrongPlayerId] ), Expect( 'Player', ('Fail', 'NoSuchThing') )
+		packet = yield GetPlayer( self.seq, [ player.id + 666] ), Expect( 'Player', ('Fail', 'NoSuchThing') )
 
 		if packet.type == 'Player':
-			self.failed( "Server does return information for non-existent PlayerId = %s!" % self.WrongPlayerId )
+			self.failed( "Server does return information for non-existent PlayerId = %s!" % ( player.id + 666 ) )
 
 class GetMultiplePlayers( AuthorizedTestSession ):
 	""" Does server return sequence of Player packets if asked about two players? """
 
 	def __iter__( self ):
+		player1 = self.ctx['players'][1]
+		player2 = self.ctx['players'][0]
+
 		GetPlayer = self.protocol.use( 'GetPlayer' )
 
-		s, p1, p2 = yield GetPlayer( self.seq, [1, 2] ), Expect( ('Sequence', 2, 'Player' ) )
+		s, p1, p2 = yield GetPlayer( self.seq, [ player1.id, player2.id ] ), Expect( ('Sequence', 2, 'Player' ) )
 
-		if p1.id != 1 or p2.id != 2:
-			self.failed( "Server returned different PlayerIds (%d,%d) than requested (1,2)." % (p1.id, p2.id) )
+		if p1.id != player1.id or p2.id != player2.id:
+			self.failed( "Server returned different PlayerIds (%d,%d) than requested (%d,%d)." % (p1.id, p2.id, player1.id, player2.id) )
 
 class PlayerTestSuite( TestSuite ):
 	__name__  = 'Players'
