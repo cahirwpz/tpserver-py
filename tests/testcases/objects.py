e4422334ed9ab023ed9d426dc8f12c3f93d169ff
Krystian Bacławski <krystian.baclawski@gmail.com>
2010-08-06 15:19:02 +0200

    Object related tests fixes and extensions.
      * setUp / tearDown for Object related tests.
      * Added GetEmptyObjectList test.

--- tests/testcases/objects.py
+++ tests/testcases/objects.py
@@ -1,5 +1,17 @@
 from test import TestSuite
-from common import AuthorizedTestSession
+from common import AuthorizedTestSession, Expect
+
+from tp.server.model import DatabaseManager, Vector3D
+
+class GetEmptyObjectList( AuthorizedTestSession ):
+	""" Sends empty list of ObjectIDs. """
+
+	NoFailAllowed = False
+
+	def __iter__( self ):
+		GetObjectsByID = self.protocol.use( 'GetObjectsByID' )
+
+		yield GetObjectsByID( self.seq, [] ), Expect( ('Fail', 'Protocol') )
 
 class GetObjectIds( AuthorizedTestSession ):
 	""" Sends some random Object related requests. """
@@ -14,6 +26,38 @@ class GetObjectIds( AuthorizedTestSession ):
 
 class ObjectTestSuite( TestSuite ):
 	__name__  = 'Objects'
-	__tests__ = [ GetObjectIds ]
+	__tests__ = [ GetEmptyObjectList, GetObjectIds ]
+
+	def setUp( self ):
+		game = self.ctx['game']
+
+		Universe, StarSystem, Planet = game.objects.use( 'Universe', 'StarSystem',	'Planet' )
+
+		universe = Universe( name = "The Universe", size = 10**8, age = 0 )
+
+		system = StarSystem(
+				name		= "The Star System",
+				parent		= universe,
+				position	= Vector3D( 0, 0, 0 ),
+				size		= 10**4)
+
+		planet = Planet(
+				name		= "The Planet",
+				parent		= system,
+				position	= Vector3D( system.position.x + 5,
+										system.position.y + 7 ),
+				size		= 10**2,
+				owner		= self.ctx['players'][0] )
+
+		self.ctx['objects'] = [ universe, system, planet ]
+
+		with DatabaseManager().session() as session:
+			for obj in self.ctx['objects']:
+				session.add( obj )
+	
+	def tearDown( self ):
+		with DatabaseManager().session() as session:
+			for obj in self.ctx['objects']:
+				obj.remove( session )
 
 __tests__ = [ ObjectTestSuite ]
