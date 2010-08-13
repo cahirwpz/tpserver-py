549aa062ff725f506f529ab9ac3b512316ae226b
Krystian Bacławski <krystian.baclawski@gmail.com>
2010-08-13 14:03:03 +0200

    Added test templates: GetWithIDMixin, GetItemIDs.
      * GetWithIDMixin should be used with every GetWithID based request. It
        automatically verifies if fetched object corresponds to an object
        stored in the Model.
      * GetItemIDs is a template to be used with GetIDSequence based
        request. It does some automatic checking of response.

--- tests/testcases/templates.py
+++ tests/testcases/templates.py
@@ -1,4 +1,6 @@
-from common import AuthorizedTestSession, ConnectedTestSession, Expect, ExpectFail, ExpectSequence, ExpectOneOf
+import time
+
+from common import AuthorizedTestSession, ConnectedTestSession, Expect, ExpectFail, ExpectSequence, ExpectOneOf, TestSessionUtils
 
 class GetWithIDWhenNotLogged( ConnectedTestSession ):#{{{
 	__request__ = None
@@ -100,6 +102,51 @@ class GetItemsWithID( AuthorizedTestSession ):#{{{
 				self.assertEqual( p, b )
 #}}}
 
+class GetWithIDMixin( object ):#{{{
+	def convert_modtime( self, packet, obj ):
+		pval = packet.modtime
+		oval = long( time.mktime( time.strptime( obj.mtime.ctime() ) ) )
+
+		return pval, oval
+
+	def assertEqual( self, packet, obj ):
+		attrs = self.__attrs__ + list( self.__attrmap__ ) + self.__attrfun__
+
+		for attr in attrs:
+			if attr in self.__attrfun__:
+				pval, oval = getattr( self, 'convert_%s' % attr )( packet, obj )
+			else:
+				objattr = self.__attrmap__.get( attr, attr )
+
+				pval = getattr( packet, attr )
+				oval = getattr( obj, objattr )
+
+			assert pval == oval, \
+					"Server responded with different %s(%d).%s (%s) than expected (%s)!" % ( self.__response__, packet.id, attr, pval, oval )
+#}}}
+
+class GetItemIDs( AuthorizedTestSession, TestSessionUtils ):#{{{
+	def __iter__( self ):
+		Request = self.protocol.use( self.__request__ )
+
+		idseq = yield Request( self.seq, -1, 0, len( self.items ) ), Expect( self.__response__ )
+
+		assert len( idseq.modtimes ) == len( self.items ), \
+				"Expected to get %d %s packets, got %d instead." % ( len( self.items ), self.__object__, len( idseq.modtimes ) )
+		assert idseq.remaining == 0, \
+				"Expected to get all %s objects, but %d left to be fetched." % ( self.__object__, idseq.remaining )
+
+		cmpId = lambda a, b: cmp( a.id, b.id )
+
+		objs = sorted( self.items, cmpId )
+
+		for item, obj in zip( sorted( idseq.modtimes, cmpId ), objs ):
+			assert item.id == obj.id, \
+					"Expected id (%s) and %s.id (%s) to be equal" % ( item.id, obj.__origname__, obj.id )
+			assert item.modtime == self.datetimeToInt( obj.mtime ), \
+					"Expected modtime (%s) and %s.mtime (%s) to be equal." % ( item.modtime, obj.__origname__, self.datetimeToInt( obj.mtime ) )
+#}}}
+
 __all__ = [ 'GetWithIDWhenNotLogged', 'GetIDSequenceWhenNotLogged',
 			'GetWithIDSlotWhenNotLogged', 'WhenNotLogged', 'GetItemWithID'
-			'GetItemsWithID' ]
+			'GetItemsWithID', 'GetWithIDMixin', 'GetItemIDs' ]
